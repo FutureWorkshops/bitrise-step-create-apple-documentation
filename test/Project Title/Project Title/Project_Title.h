936068a807d03a78fba7f5f356c132e3e8088259
Igor Ferreira <igorcferreira@hotmail.com>
2020-06-12 11:58:32 +0200

    Create project on testing to showcase more complex project naming

---
+++ test/Project Title/Project Title/Project_Title.h
@@ -0,0 +1,19 @@
+//
+//  Project_Title.h
+//  Project Title
+//
+//  Created by Igor Ferreira on 12/06/2020.
+//  Copyright © 2020 Future Workshops. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Project_Title.
+FOUNDATION_EXPORT double Project_TitleVersionNumber;
+
+//! Project version string for Project_Title.
+FOUNDATION_EXPORT const unsigned char Project_TitleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Project_Title/PublicHeader.h>
+
+
